2f544e4628ef683bb3ad4176018103e722550dab
Sherlock Guo <sherlock.guo@go6d.com>
2016-12-21 17:49:52 +0800

    mochawesome report file name changed, adding exception for report director

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import * as Mocha from 'mocha'
 let date = new Date()
 
-let mocha = new Mocha({ 
-  timeout: 15000,
+let mocha: any = new Mocha ({
+  timeout: 5000,
   reporter: 'mochawesome'
 })
+
+// To avoid typescript's type check
+let reporterOptions = {
+  reportDir: `../test_report/${date.toLocaleString().replace(/\//g, '-')}`,
+  reportName: 'apiTestReport'
+}
+
+mocha.options.reporterOptions = reporterOptions
 
 mocha.addFile('./test_api')
 mocha.run()
