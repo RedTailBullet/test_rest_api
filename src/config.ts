100b0afbe0141e1ddf38f216ab613bf58017025c
Sherlock Guo <sherlock.guo@go6d.com>
2016-12-22 20:36:23 +0800

    a small fix

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export const ENV_CLIENT_ID_NAME = 'CTP_CLIENT_ID'
 export const ENV_CLIENT_SECRET_NAME = 'CTP_CLIENT_SECRET'
 export const ENV_PROJECT_KEY_NAME = 'CTP_PROJECT_KEY'
 
-// export const CTP_BASEURL = 'https://api.sphere.io'
-export const CTP_BASEURL = 'http://localhost:8088'
+export const CTP_BASEURL = 'https://api.sphere.io'
+// export const CTP_BASEURL = 'http://localhost:8088'
 
 // export const NEED_ACCESS_TOKEN = true
 export const NEED_ACCESS_TOKEN = false
